529bc4ff1d1e2a0193cc5b92b0810fba0000457a
cl-zani <carlos.zani@fiocruz.br>
2026-06-24 10:42:57 -0300

    Update API_URL in config.js
    Signed-off-by: cl-zani <carlos.zani@fiocruz.br>

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CENTRAL DE CONTROLE - BARRA LINDA
 // Toda manhã, mude apenas o link abaixo e salve o arquivo.
 const CONFIG = {
-    API_URL: 'https://consultant-mpg-far-gas.trycloudflare.com'
+    API_URL: 'https://how-bond-patricia-useful.trycloudflare.com'
 };
