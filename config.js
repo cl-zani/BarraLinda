2146959fc7c9139217c2711897f74183fa5bbf03
cl-zani <carlos.zani@fiocruz.br>
2026-06-26 13:05:26 -0300

    Update API_URL in config.js
    Signed-off-by: cl-zani <carlos.zani@fiocruz.br>

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // CENTRAL DE CONTROLE - BARRA LINDA
 // Toda manhã, mude apenas o link abaixo e salve o arquivo.
 const CONFIG = {
-    API_URL: 'https://how-bond-patricia-useful.trycloudflare.com'
+    API_URL: 'https://variance-model-powell-helicopter.trycloudflare.com'
 };
